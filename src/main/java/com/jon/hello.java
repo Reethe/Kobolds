fd2a5e7929d838c843f1b402b6774bb2881c8a45
Jonathan Soncrant <JonathCareer@hotmail.com>
2019-08-21 10:23:44 -0400

    Whatever

---
+++ src/main/java/com/jon/hello.java
@@ -0,0 +1,22 @@
+package com.jon; 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class hello extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+			PrintWriter pw = resp.getWriter();
+			
+			pw.write("Hello World");
+	}
+}
+
+
+//
+//git clone httpsyourrepository.
